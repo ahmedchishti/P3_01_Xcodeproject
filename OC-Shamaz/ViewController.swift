300a7631d0304db70f257c0dac58cb3003ae4178
Ahmed Chishti <ahmedchishti2k@gmail.com>
2020-09-30 16:19:34 -0700

    Initial Commit

---
+++ OC-Shamaz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OC-Shamaz
+//
+//  Created by Ahmed Chishti on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
